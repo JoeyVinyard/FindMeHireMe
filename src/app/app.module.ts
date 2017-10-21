eefbebfe5eedbddf6f4f8d1296f1a88896850fa0
Joey Vinyard <jviny98@yahoo.com>
2017-10-21 01:30:49 -0400

    Project structure built out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+
+import { routes } from './res/routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/res/routes.ts
@@ -0,0 +1,7 @@
+import {SplashComponent} from '../splash/splash.component';
+export const routes = [
+	{
+		path: '',
+		component: SplashComponent
+	}
+];
