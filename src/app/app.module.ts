91a2fadcb3d2ea8320b76447fc091ed07da6b670
Joey Vinyard <jviny98@yahoo.com>
2017-10-21 03:55:31 -0400

    Finished signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
 
 import { routes } from './res/routes';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashComponent
+    SplashComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-splash',
-  templateUrl: './splash.component.html',
-  styleUrls: ['./splash.component.css']
+	selector: 'app-splash',
+	templateUrl: './splash.component.html',
+	styleUrls: ['./splash.component.css']
 })
 export class SplashComponent implements OnInit {
 
-  constructor() { }
+	errors: any = {}; //This one is public so that angular can access it
+	model: any = {};  //Model that angular will store data in
+	valid = true;
 
-  ngOnInit() {
-  }
 
+
+	constructor(){}
+
+	validateEmail(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+
+	validate(){
+		this.errors = {};
+		if(!this.model.email){
+			this.errors.email = "Please provide an email";
+		}else if(!this.validateEmail(this.model.email)){
+			this.errors.email = "Please enter a valid email";
+		}
+		if(!this.model.pass){
+			this.errors.pass = "Please provide a password";
+		}else if(this.model.pass.length<6){
+			this.errors.pass = "Password must be at least 6 characters long";
+		}
+		if(!this.model.confpass){
+			this.errors.confpass = "Please confirm your password";
+		}else if(this.model.pass != this.model.confpass){
+			this.errors.confpass = "Passwords must match";
+		};
+
+		return !!Object.keys(this.errors).length;
+	}
+
+	onSubmit(){
+		this.valid = this.validate();
+		if(this.valid){
+			return;
+		}
+		//submit
+	}
+
+	ngOnInit() {}
 }
